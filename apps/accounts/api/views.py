a920da47a75460cc33ab2aa2f771bb07b4412ac7
Saroj Paudel <sarojpaudel79@gmail.com>
2021-02-03 19:38:58 +0545

    abc# Please enter the commit message for your changes. Lines starting

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -57,9 +57,10 @@ class VendorLoginUserView(LoginView):
         serializer = CustomLoginSerializer(data=data)
         data['user_type'] = request.user.user_type
         user_type = data['user_type']
-        if user_type is 2:
+
+        if user_type == 2:
             serializer.is_valid(raise_exception=True)
-            new_data = serializer.data
+            # new_data = serializer.data
             user = serializer.validated_data["user"]
             serializer = self.get_serializer(user)
             token, created = Token.objects.get_or_create(user=user)
@@ -68,10 +69,11 @@ class VendorLoginUserView(LoginView):
                                       "serializer.data": serializer.data},
                                        status=status.HTTP_200_OK)
 
-        # else:
-        #     message = "This is not a seller account"
-        #     return Response({'message':message,},
-        #                     status=status.HTTP_400_BAD_REQUEST)
+        else:
+            print(user_type)
+            message = "This is not a seller account"
+            return Response({'message':message,},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class Logout(GenericAPIView):
