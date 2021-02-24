b3040abffb4f088e4c35d6e6a6f07738a7d2a9e9
sarojpaudel79 <sarojpaudel79@gmail.com>
2021-02-24 17:49:39 +0545

    addtoorder

--- apps/orders/api/views.py
+++ apps/orders/api/views.py
@@ -119,48 +119,90 @@ class OrderItemView(ListAPIView):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
 
-class AddtoOrderItemView(APIView):
+class AddressAPIView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
+    queryset = BillingDetails.objects.all()
+    serializer_class = BillingDetailsSerializer
+
+    def get_queryset(self):
+        abc = BillingDetails.objects.filter(user=self.request.user)
+        return abc
+
+    # def perform_create(self, serializer):
+    #     try:
+    #         abc = BillingDetails.objects.(user=self.request.user)
+    #         # return wishlist
+    #         return Response({"message": "Wishlist Already existed",
+    #                          "wishlist": abc
+    #                          },
+    #                         status=status.HTTP_200_OK
+    #                         )
+    #
+    #     except ObjectDoesNotExist:
+    #         user = self.request.user
+    #         serializer.save(user=user)
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
+
+
+class AddtoOrderView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = BillingDetails.objects.all()
+    serializer_class = BillingDetailsSerializer
+
+    #def perform_create(self, serializer):
+
+
+
+# class AddtoOrderItemView(APIView):
+#     permission_classes = [IsAuthenticated]
     # queryset = OrderItem.objects.all()
     # serializer_class = OrderItemSerializer
     # @action(detail=True, methods=['post'])
 # @login_required
-    def post(self, request, pk):
-        item = get_object_or_404(Product, pk=pk)
-        order_item, created = OrderItem.objects.get_or_create(
-            item=item,
-            user=self.request.user,
-            ordered=False
-        )
-        order_qs = Order.objects.filter(user=self.request.user, ordered=False)
-
-        if order_qs.exists():
-            order = order_qs[0]
-
-            if order.items.filter(item__pk=item.pk).exists():
-                order_item.quantity += 1
-                order_item.save()
-                return Response({"message": "Quantity is added",
-                                 },
-                                status=status.HTTP_200_OK
-                                )
-            else:
-                order.items.add(order_item)
-                return Response({"message": " Item added to your cart", },
-                                status=status.HTTP_200_OK,
-                                )
-        else:
+#     def post(self, request, pk):
+#         item = get_object_or_404(Product, pk=pk)
+#         order_item, created = OrderItem.objects.get_or_create(
+#             item=item,
+#             user=self.request.user,
+#             ordered=False
+#         )
+#         order_qs = Order.objects.filter(user=self.request.user, ordered=False)
+#
+#         if order_qs.exists():
+#             order = order_qs[0]
+#
+#             if order.items.filter(item__pk=item.pk).exists():
+#                 order_item.quantity += 1
+#                 order_item.save()
+#                 return Response({"message": "Quantity is added",
+#                                  },
+#                                 status=status.HTTP_200_OK
+#                                 )
+#             else:
+#                 order.items.add(order_item)
+#                 return Response({"message": " Item added to your cart", },
+#                                 status=status.HTTP_200_OK,
+#                                 )
+#         else:
             # ordered_date = datetime.now()
             # order = Order.objects.create(user=self.request.user, ordered_date=ordered_date)
             # order.items.add(order_item)
             # return Response({"message": "Order is created & Item added to your cart", },
             #                 status=status.HTTP_200_OK,
             #                 )
-            abc = BillingDetailsSerializer(order_qs,many=True)
-            return Response ({"message": "Order is created & Item added to your cart",
-                              "data" : abc.data},
-                             status=status.HTTP_200_OK,
-                             )
+            #serializer = BillingDetailsSerializer(data=request.data)
+            # if serializer.is_valid():
+            #     serializer.save()
+            #     return Response ({"message": "Order is created & Item added to your cart",
+            #                   "data" : serializer.data},
+            #                  status=status.HTTP_200_OK,
+            #                  )
+            #
+            # else:
+            #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class DelOrderItemView(DestroyAPIView,):
     permission_classes = [IsAuthenticated]
